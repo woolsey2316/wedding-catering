2121f19dc8408955a4817ecfcfc482b391853b60
woolsey2316 <woolsey2316@gmail.com>
2020-11-12 23:21:07 +1100

    blog category page

--- components/Category.tsx
+++ components/Category.tsx
@@ -12,7 +12,7 @@ export function Category({ title, amount } : Props) {
   return (
     <div className="cursor-pointer border-b border-gray-400 flex items-center py-2 text-gray-900 hover:text-coral transition duration-300 ease-in-out">
       <FontAwesomeIcon className="text-xs mr-2 py-2px" icon={faChevronRight}/>
-      <p className="font-rubik">{title}</p>
+      <a href={`/category/${title}`} className="font-rubik">{title}</a>
       <p className="font-rubik font-medium italic text-gray-500 ml-auto">{`(${amount.toString()})`}</p>
     </div>
   )
---
+++ components/blog/DetailedBlogLinkThumbnail.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export interface Props {
+  key: number;
+  link: string;
+  image: string;
+  title: string;
+  date?: string;
+  index?: number;
+}
+
+interface Blog {
+  image: string;
+  link: string;
+  title: string;
+  author: string;
+  date: string;
+  category: string;
+}
+
+function DetailedBlogLinkThumbnail({blog, key}) {
+  return (
+    <div onClick={() => {window.location.href = blog.link}} className="grid grid-cols-12 mt-6 pb-8 border-b border-gray-300">
+      <img className="max-w-full height-auto w-full col-span-6 cursor-pointer hover:opacity-75 transition-opacity duration-300 ease-in-out mb-4" src={`/${blog.image}`}/>
+      <div className="col-start-8 col-span-5 pl-5">
+        <p className="text-coral text-sm mb-2">{blog.category}</p>
+        <h4 className="font-nunito font-bold uppercase leading-snug text-lg text-gray-900 cursor-pointer hover:text-coral transition-colors duration-300 ease-in-out">{blog.title}</h4>
+        <div className="flex mt-4 items-center">
+          <p className="font-rubik text-gray-600 mr-1">by</p> 
+          <p className="cursor-pointer text-sm font-rubik text-gray-900 hover:text-coral transition-color duration-300 ease-in-out">{blog.author}</p>
+          <p className="font-rubik text-gray-600 text-xs mx-4">|</p>
+          <p className="font-rubik text-sm text-gray-600">{blog.date}</p>
+        </div>
+        <div className="border-b border-coral w-16 pb-4"/>
+        <p className="text-gray-900 font-rubik mt-4 text-sm">Lommodo ligula eget dolor. Aenean massa. Cum sociis que penatibus et magnis dis parturient montes lorem, nascetur ridiculus mus. Donec quam felis, ultricies nec, 
+          pellentesque eu, pretium quis, sem. Nulla…
+        </p>
+      </div>
+    </div>
+  )
+}
+export { DetailedBlogLinkThumbnail }
