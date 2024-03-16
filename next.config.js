1a7c3bae2da9e6b23ba68aee8eff34f3b9dcf3ce
David Woolsey <woolsey2316@gmail.com>
2024-03-16 18:45:30 +1100

    deploy

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/
-});
+// const withMDX = require("@next/mdx")({
+//   extension: /\.mdx?$/
+// });
 
-module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  publicRuntimeConfig: {
-    APP_NAME: 'SEOBLOG',
-    API_DEVELOPMENT: 'http://localhost:8000/api',
-    PRODUCTION: false
-  },
-  target: 'serverless',
-});
+// module.exports = withMDX({
+//   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+//   publicRuntimeConfig: {
+//     APP_NAME: 'SEOBLOG',
+//     API_DEVELOPMENT: 'http://localhost:8000/api',
+//     PRODUCTION: false
+//   }
+// });
 
